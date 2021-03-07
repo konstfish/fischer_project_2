cc5dcf3afa7ad67592f4cbc26b8328dda364a7f9
David Fischer <david@konst.fish>
2021-03-07 16:06:02 +0100

    Include simple CLA handling using CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,70 @@ catnr:  03
 
 #include "pop3client.h"
 
-#include <iostream>
-
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include "spdlog/sinks/basic_file_sink.h"
 
+#include "CLI11.hpp"
+
+
+#include <iostream>
+
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    // CLI11 setup
+    CLI::App app("POP3 Client");
+
+    string servername = "";
+    app.add_option("-s,--server", servername, "Server Domain");
+
+    string username = "";
+    app.add_option("-u,--user", username, "Account Name");
+
+    string password = "";
+    app.add_option("-p,--pass", password, "Account Password");
+
+    bool use_tls{false};
+    app.add_flag("-t, --tls", use_tls, "Connect using TLS");
+
+    int port{-1};
+    app.add_option("--port", port, "Specify custom port");
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) { 
+        return app.exit(e);
+    }
+
+    // set port dynamically if not specified
+    if(port == -1){
+        if(use_tls){
+            port = 995;
+        }else{
+            port = 110;
+        }
+    }
+
     auto console = spdlog::stdout_color_mt("console");
     auto logger = spdlog::basic_logger_mt("logger", "logs/basic-log.txt");
 
     spdlog::get("logger")->info("Starting POP3 Client");
 
     //pop3client c("fortimail.konst.fish", 110, false);
-    pop3client c("fortimail.konst.fish", 995, true);
-    c.establish_connection();
-    c.login("user@nvs.com", "12345678");
-
+    //pop3client c("fortimail.konst.fish", 995, true);
+    cout << servername << endl;
+    pop3client c(servername, port, use_tls);
+    int check = 0;
+    check = c.establish_connection();
+    if(check){
+        return 1;
+    }
+    //c.login("user@nvs.com", "12345678");
+    check = c.login(username, password);
+    if(check){
+        return 1;
+    }
     c.get_total_messages();
     //c.delete_message(5);
     c.retrieve_messages(4);
